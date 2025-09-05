05c8f5d29ec248fec5a1d3c529d5ff54653ea11e
calebsytsma-cloud <caleb.sytsma@tamu.edu>
2025-09-05 00:18:31 -0500

    Update MEEN_432_Proj1Part1_Script.m
    fixed issues, added error

--- MEEN_432_Proj1Part1_Script.m
+++ MEEN_432_Proj1Part1_Script.m
@@ -1,21 +1,28 @@
-time_step=1
-sim(MEEN_432_Proj1Part1)
-time = out.tout;           % Time array
-w_dot = out.w_dot.data;    % Data from w_dot timeseries
-w_k   = out.w_k.data;      % Data from w_k timeseries
+time_step = 0.01;
+J = 1;
+tau = 10;
+b = 1;
+w_initial = 0;
 
+out = sim('MEEN_432_Proj1Part1');
 
+time = out.tout;
+w_dot = out.w_dot.data;    
+w_k   = out.w_k.data;      
 
+% Analytic solution (vectorized)
+true_w = (tau/b) * (1 - exp(-b*time/J)) + w_initial * exp(-b*time/J);
 
-   
+% Error
+error = w_k - true_w;   
 
 figure;
 plot(time, w_dot, 'LineWidth', 1.5);
 hold on;
 plot(time, w_k, '--', 'LineWidth', 1.5);
+plot(time, error, ':', 'LineWidth', 1.5);
 xlabel('Time (s)');
 ylabel('Values');
-legend('w\_dot', 'w\_k');
+legend('w\_dot', 'w\_k', 'error');
 title('Simulation Outputs');
 grid on;
-
